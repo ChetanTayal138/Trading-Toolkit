44b33910508b56ddb25a9534f5caaff90e87d8d4
ChetanTayal138 <chetantayal138@gmail.com>
2021-03-19 16:09:06 +0530

    Fixed ema/sma implementation to account for lag

--- indicators/utils.py
+++ indicators/utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def read_df(filename):
-    df = pd.read_csv(filename, parse_dates=["Date"])
+    df = pd.read_csv(filename, usecols=['Date', 'Open', 'High', 'Low', 'Close'], header=0, parse_dates=['Date'])
     return df
 
 def filter_df(START_DATE, END_DATE, df):    
@@ -16,9 +16,9 @@ def filter_df(START_DATE, END_DATE, df):
 def simple_moving_average(df, day_range=10, view=False):
     j=0
     means = {}
-    for i in range(0,len(df)):
-        
+    for i in range(0,len(df)):        
         temp_df = df.iloc[i:i+day_range, :]
+        
         if view:
             print(len(temp_df))
             print(f"First day of window ---> {temp_df.iloc[0, 0]}")
@@ -36,17 +36,27 @@ def exponential_moving_average(df, day_range=10, view=False, smoothing_factor=2)
     #Giving more weight to recent prices using smoothing factor = (s/(selected_time_period+1))
     j = 0
     means = {}
-    print(smoothing_factor/(day_range+1))
+    multiplier = (smoothing_factor/(day_range+1))
+    
+
     for i in range(0, len(df)):
         
         temp_df = df.iloc[i:i+day_range, :]
+    
+        
+        
+        if(i == 0):
+            curr_mean = temp_df['Close'].mean()
 
-        curr_mean = temp_df['Close'].mean()
-
-        if i > 1:
+        else:
             prev_mean = means[j-1]['Mean']
+            close = temp_df.tail()['Close'].values[-1]
+            
+            curr_mean = (close - prev_mean) * multiplier + prev_mean
+            
+            #curr_mean = curr_mean * (smoothing_factor / (day_range+1)) +  prev_mean * (1- (smoothing_factor/(day_range+1))) #Giving more weight to current means
+            
 
-            curr_mean = curr_mean * (smoothing_factor / (day_range+1)) +  prev_mean * (1- (smoothing_factor/(day_range+1))) #Giving more weight to current means
 
         means[j] = {"Mean":curr_mean, "First":temp_df.iloc[0,0], "Last":temp_df.iloc[len(temp_df)-1, 0]}
 
@@ -58,40 +68,47 @@ def exponential_moving_average(df, day_range=10, view=False, smoothing_factor=2)
 
 if __name__ == "__main__":
 
-    df = read_df("data/tsla.csv")
-    
-    START_DATE = "2020-01-01"
+    df = read_df("../data/nasdaq/tsla.csv")
+        
+    START_DATE = "2015-01-01"
     END_DATE = "2021-01-01"
-    
+    DAY_RANGE = 50
+     
     temp = filter_df(START_DATE, END_DATE, df)
     
-    sma = simple_moving_average(temp, day_range=30)
-
-    ema = exponential_moving_average(temp, day_range=30)
+    temp = temp.dropna()
     
+    sma = simple_moving_average(temp, day_range=DAY_RANGE)
+    
+    ema = exponential_moving_average(temp, day_range=DAY_RANGE)
+
     sma_means = []
     ema_means = []
+    
     for k in sma:
         sma_means.append(sma[k]['Mean'])
 
     for k in ema:
         ema_means.append(ema[k]['Mean'])
 
-    
     sma_means = np.array(sma_means)
     ema_means = np.array(ema_means)
-    
-    
-    actual_price = np.array(temp['Close'])
 
-    print(np.square(actual_price-sma_means).mean(axis=0))
-    print(np.square(actual_price-ema_means).mean(axis=0))
-
+    temp = temp.iloc[DAY_RANGE:, :]
+    ema_means = ema_means[:-DAY_RANGE]
+    sma_means = sma_means[:-DAY_RANGE]
     
-    plt.title('Simple Moving Average (Red) vs Exponential Moving Average (Green)')
-    plt.plot(sma_means, 'r')
+    temp[f'{DAY_RANGE}-EMA'] = ema_means
+    temp[f'{DAY_RANGE}-SMA'] = sma_means
+
+    actual_price = np.array(temp['Close'])[DAY_RANGE:]
+
+    print(temp.head())
+    print(temp.tail())
+    plt.title(f'Closing (Black) vs {DAY_RANGE}-Day EMA (Red) vs {DAY_RANGE}-Day SMA (Green)')
+    plt.plot(sma_means, 'green')
     plt.plot(np.array(temp['Close']), 'black')
-    plt.plot(ema_means, 'g')
+    plt.plot(ema_means, 'red')
     plt.show()
 
 
